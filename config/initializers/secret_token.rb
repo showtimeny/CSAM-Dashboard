4b5c7f6169c2b860f0c3aa47017192056a9f5413
Jackson Coakley <jackson.coakley@gmail.com>
2011-10-19 16:41:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsamDashboard::Application.config.secret_token = '085d12be944f5be156da7cd3f5d07c081be829ba63a098148073b0750d30cbbb1e367d0f8725da7aacf2d35c769633a3f5606b3db667c13d3ddc7c7eb294faf7'
